2b1e36a275ad62d1020e2d2566ba670134b34746
barnus <MolnBarn47@stud.uni-obuda.hu>
2023-12-14 07:49:57 +0100

    column deleted

--- fifa.py
+++ fifa.py
@@ -7,7 +7,7 @@ pickled_model = pickle.load(open('./models/fifa_classifier_model_dtree2.pkl', 'r
 st.set_page_config(layout="wide")
 st.title('FIFA cards classification')
 id=246621
-col1, col2, col3 , col4,col5, col6,col7,col8,col9 = st.columns([0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1],gap="small")
+col1, col2, col3 , col4,col5, col6,col7,col8 = st.columns([0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1],gap="small")
 with col1:
     st.header(":green[Player details]",)
     name =hash(st.text_input("Name"))
@@ -20,10 +20,6 @@ with col1:
     weekfoot = st.slider('Week foot',min_value=1, max_value=100,value=50)
     skill = st.slider('Skill moves',min_value=1, max_value=100,value=50)
 with col2:
-    st.header(":green[Pace:]")
-    sprint = st.slider('Sprint speed',min_value=1, max_value=100,value=50)
-    accel = st.slider('Acceleration',min_value=1, max_value=100,value=50)
-with col3:
     st.header(":green[Shooting]")
     attposition = st.slider('Att. Position',min_value=1, max_value=100,value=50)
     finish = st.slider('Finising',min_value=1, max_value=100,value=50)
@@ -31,7 +27,7 @@ with col3:
     longshot = st.slider('Long shot',min_value=1, max_value=100,value=50)
     volley = st.slider('Volleys',min_value=1, max_value=100,value=50,)
     penalty = st.slider('Penalty',min_value=1, max_value=100,value=50)
-with col4:
+with col3:
     st.header(":green[Passing]",)
     vision = st.slider('Vision',min_value=1, max_value=100,value=50,)
     cross = st.slider('Crossing',min_value=1, max_value=100,value=50)
@@ -39,7 +35,7 @@ with col4:
     shortpass = st.slider('ShortPassing',min_value=1, max_value=100,value=50)
     longpass = st.slider('Longpass',min_value=1, max_value=100,value=50)
     curve = st.slider('Curve',min_value=1, max_value=100,value=50)
-with col5:
+with col4:
     st.header(":green[Dribling:]")
     agility = st.slider('Agility',min_value=1, max_value=100,value=50)
     balance = st.slider('Balance',min_value=1, max_value=100,value=50)
@@ -47,21 +43,24 @@ with col5:
     ballcont = st.slider('Ball control',min_value=1, max_value=100,value=50)
     dribling = st.slider('Dribling',min_value=1, max_value=100,value=50)
     composure = st.slider('Composure',min_value=1, max_value=100,value=50)
-
-with col6:
+with col5:
     st.header(":green[Defending:]")
     intercept = st.slider('Interception',min_value=1, max_value=100,value=50)
     headacc = st.slider('Head accuracy',min_value=1, max_value=100,value=50)
     standtack = st.slider('Standing Tackle',min_value=1, max_value=100,value=50)
     slidtack = st.slider('Sliding Tackle',min_value=1, max_value=100,value=50)
     marking = st.slider('Marking',min_value=1, max_value=100,value=50)
-with col7:
+
+with col6:
     st.header(":green[Physical:]")
     jump = st.slider('Jump',min_value=1, max_value=100,value=50)
     stamina = st.slider('Stamina',min_value=1, max_value=100,value=50)
     strength = st.slider('Strength',min_value=1, max_value=100,value=50)
     aggr = st.slider('Aggressive',min_value=1, max_value=100,value=50)
-with col8:
+    st.header(":green[Pace:]")
+    sprint = st.slider('Sprint speed',min_value=1, max_value=100,value=50)
+    accel = st.slider('Acceleration',min_value=1, max_value=100,value=50)
+with col7:
     st.header(":green[Goalkeeper attributes:]")
     goaliecheckbox=st.checkbox(label="Are you a goalkeeper?")
     
@@ -77,7 +76,6 @@ with col8:
         gkkick = st.slider('Goalkeeper Kicking',min_value=1, max_value=100,value=50)
         gkpos = st.slider('Goalkeeper Positioning',min_value=1, max_value=100,value=50)
         gkref = st.slider('Goalkeepe Reflex',min_value=1, max_value=100,value=50)
-
 input_data = [[id,name,age,value,wage,special,international,weekfoot,skill,
                 cross,finish,headacc,shortpass,volley,dribling,curve,fkacc,longpass,ballcont,accel,
                 sprint,agility,react,balance,shotp,jump,stamina,strength,
@@ -102,7 +100,7 @@ def ovr_calc():
     return round((def_calc(intercept,headacc,standtack,slidtack,marking)+
 phy_calc(jump,stamina,strength,aggr)+dri_calc(agility,balance,react,ballcont,dribling,composure)+pas_calc(vision,cross,fkacc,shortpass,longpass,curve)+
 sho_calc(attposition,finish,shotp,longshot,volley,penalty)+pace_calc(sprint,accel))/6)
-with col9:
+with col8:
     if st.button('Create Fifa card',type="primary"):
         st.write(id+1)
         card_prediction = pickled_model.predict(input_data)
